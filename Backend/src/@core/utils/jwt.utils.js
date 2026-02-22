98549b6bdb6a222c8d21f80152fee2d3313c468c
Haritha-cd <harithapawan12@gmail.com>
2026-02-22 11:25:16 +0530

    move .git file

--- Backend/src/@core/utils/jwt.utils.js
+++ Backend/src/@core/utils/jwt.utils.js
@@ -44,3 +44,14 @@ export const extractTokenFromHeader = (authHeader) => {
   }
   return authHeader.split(' ')[1];
 };
+
+import bcrypt from "bcryptjs";
+
+export const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+export const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-
+import authRoutes from './modules/auth/auth.routes.js';
 const app = express();
 
 app.use(cors());
@@ -15,5 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.json({ message: 'Handwash+ Backend Running ' });
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
