97e5cc8ed1c543f60dcdfdd37eddee528563a47d
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-23 22:16:27 +0530

    Add getById route path

--- Backend/src/modules/students/student.routes.js
+++ Backend/src/modules/students/student.routes.js
@@ -2,9 +2,10 @@ import express  from "express";
 const router = express.Router();
 
 //Insert model
-import {addStudent, getAllStudents,} from "./student.controller.js";
+import {addStudent, getAllStudents,getById} from "./student.controller.js";
 
 router.get("/",getAllStudents);
 router.post("/",addStudent);
+router.get("/:id",getById);
 
 export default router;
