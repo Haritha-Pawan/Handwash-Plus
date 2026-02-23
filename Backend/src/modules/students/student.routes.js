5b8e3acf0b1a8295d1bcc8f91361e7696800e7a3
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-23 22:42:37 +0530

    Implement put endpoint

--- Backend/src/modules/students/student.routes.js
+++ Backend/src/modules/students/student.routes.js
@@ -2,10 +2,11 @@ import express  from "express";
 const router = express.Router();
 
 //Insert model
-import {addStudent, getAllStudents,getById} from "./student.controller.js";
+import {addStudent, getAllStudents,getById,updateStudent} from "./student.controller.js";
 
 router.get("/",getAllStudents);
 router.post("/",addStudent);
 router.get("/:id",getById);
+router.put("/:id",updateStudent);
 
 export default router;
