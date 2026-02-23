00e016dfcc388c06eb217b8e9c2f3b64fd69e669
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-23 22:57:47 +0530

    Implement Delete endpoint

--- Backend/src/modules/students/student.routes.js
+++ Backend/src/modules/students/student.routes.js
@@ -2,11 +2,12 @@ import express  from "express";
 const router = express.Router();
 
 //Insert model
-import {addStudent, getAllStudents,getById,updateStudent} from "./student.controller.js";
+import {addStudent, deleteStudent, getAllStudents,getById,updateStudent} from "./student.controller.js";
 
 router.get("/",getAllStudents);
 router.post("/",addStudent);
 router.get("/:id",getById);
 router.put("/:id",updateStudent);
+router.delete("/:id",deleteStudent);
 
 export default router;
