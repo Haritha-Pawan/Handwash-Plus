33693395525087fadf09a26f6ec26f5fd012d3bf
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-23 18:45:19 +0530

    Add Update controller method

--- Backend/src/modules/classrooms/classroom.controller.js
+++ Backend/src/modules/classrooms/classroom.controller.js
@@ -22,13 +22,57 @@ export const addClassroom = async(req, res) =>{
     
 
     try{
-const classroom = await Classroom.create({name,grade,schoolId,teacherId});
+    const classroom = await Classroom.create({name,grade,schoolId,teacherId});
     return res.status(200).json({classroom});  
+
     }catch(err){
         console.log(err);
         return res.status(500).json({ message: "Unable to add classroom" });
     }
 
+};
+//get by id
    
+ export const getById = async(req,res) =>{
+     const id = req.params.id;
 
-}
+     let classroom;
+
+     try{
+        classroom = await Classroom.findById(id);
+     }catch(err){
+        console.log(err);
+     }
+
+     //no available classroom
+
+     if(!classroom){
+        return res.status(404).json({ message: "Classroom not found" });
+     }
+      return res.status(200).json({ classroom});
+ };
+
+ //update classroom details
+
+    export const updateClassroom = async(req,res) =>{
+        const id = req.params.id;
+
+         const {name,grade,schoolId,teacherId} = req.body;
+
+         let classroom;
+
+         try{
+            classroom = await Classroom.findByIdAndUpdate(id,
+                 { name, grade, schoolId, teacherId },
+                 { new: true, runValidators: true }
+            );
+             if (!classroom) return res.status(404).json({ message: "Classroom not found" });
+        return res.status(200).json({ classroom});
+         }catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+    }
+    };
+
+   
+  
