1792fd3cfb990929f5d6c28204473f8c777d66f3
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-23 18:48:44 +0530

    Add update route

--- Backend/src/modules/classrooms/classroom.routes.js
+++ Backend/src/modules/classrooms/classroom.routes.js
@@ -2,10 +2,13 @@ import express from "express";
 const router = express.Router();
 
 //Insert model
-import {addClassroom, getAllClassrooms} from "./classroom.controller.js";
+import {addClassroom, getAllClassrooms, getById, updateClassroom} from "./classroom.controller.js";
 
 router.get("/",getAllClassrooms);
 router.post("/",addClassroom);
+router.get("/:id",getById);
+router.put("/:id",updateClassroom);
+
 
 //export
 //module.exports=router;
