9fe483f6ce20bd4e4b986b92616f81d97010dda0
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-02-24 18:05:33 +0530

    feat: implement JWT authentication middleware for user authorization

--- Backend/src/@core/middleware/auth.middleware.js
+++ Backend/src/@core/middleware/auth.middleware.js
@@ -1,4 +1,23 @@
-import { verifyToken,extractTokenFromHeader } from "../utils/jwt.utils.js";
-import {ROLES} from "../utils/constants.js";
+import jwt from "jsonwebtoken";
+import config from "../../config/index.js";
 
-const 
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    req.user = decoded; // { id, role }
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
+
