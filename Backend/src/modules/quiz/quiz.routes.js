07f317325e4c4900112e397ba3bed0274ab85de1
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-25 00:46:47 +0530

    feat(quiz):Add Put endpoint

--- Backend/src/modules/quiz/quiz.routes.js
+++ Backend/src/modules/quiz/quiz.routes.js
@@ -1,8 +1,9 @@
 import express  from "express";
-import { createQuiz, getQuizzesByClassroom } from "./quiz.controller.js";
+import { createQuiz, getQuizzesByClassroom, UpdateQuiz } from "./quiz.controller.js";
 const router = express.Router();
 
 router.post("/",createQuiz);
 router.get("/classroom/:classroomId",getQuizzesByClassroom);
+router.put('/:id',UpdateQuiz);
 
 export default router;
