a2eee24aaf99aee1b86ba68406d389566f57d331
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-02-24 18:05:19 +0530

    feat: add user routes and mongoose connection to app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import authRoutes from './modules/auth/auth.routes.js';
+import postRoutes from './modules/post/post.routes.js';
+import userRoutes from './modules/users/user.routes.js';
+import mongoose from 'mongoose';
 const app = express();
 
 app.use(cors());
@@ -11,11 +14,21 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB:", mongoose.connection.name);
+});
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'Handwash+ Backend Running ' });
 });
-
+// auth Routes
 app.use("/api/auth", authRoutes);
+// post Routes
+app.use("/api/posts", postRoutes);
+// user Routes
+app.use("/api/users", userRoutes);
+
+
 
 export default app;
