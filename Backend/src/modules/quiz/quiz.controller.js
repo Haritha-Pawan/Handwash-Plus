803465f70078749933933d6e397b737e1798c5ba
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-24 22:46:32 +0530

    feat(quiz):Add insert method

---
+++ Backend/src/modules/quiz/quiz.controller.js
@@ -0,0 +1,44 @@
+import { Quiz } from "./quiz.model.js";
+import { Classroom} from "../classrooms/classroom.model.js";
+
+//create quiz
+
+export const createQuiz = async (req,res) =>{
+    try{
+
+        const {title, classroomId, teacherId, questions } = req.body;
+
+        //validation
+        if(!title || !classroomId || ! questions || questions.length == 0){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        //find classroom
+        const classroom = await Classroom.findById(classroomId);
+
+        if(!classroom){
+             return res.status(404).json({message:"classroom not found"});
+
+        }
+
+        //check teacher is assign to the classsroom
+         if (classroom.teacherId.toString() !== teacherId) {
+         return res.status(403).json({ message: "This teacher is not assigned to the classroom" });
+         }
+        //create quiz
+
+        const quiz = await Quiz.create({
+            title,
+            classroomId,
+            teacherId,
+            questions
+        });
+
+        res.status(201).json({
+            message:"Quiz created successfuly",
+            quiz
+        });
+    }catch (error){
+        res.status(500).json({ message: error.message });
+    }
+};
