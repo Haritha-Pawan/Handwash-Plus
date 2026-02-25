cf9d11cb261f7ec55c3db9eeeaced7256994ccd7
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-25 22:25:43 +0530

    feat(classroomBottles):Add updatebottles

---
+++ Backend/src/modules/classroomBottles/classroomBottles.controller.js
@@ -0,0 +1,37 @@
+import DistributedBottles from "./distributedBottles.model.js";
+import ClassroomBottles from "./classroomBottles.model.js";
+import mongoose from "mongoose";
+
+export const updateClassroomBottles = async(req,res) =>{
+    const {classroomId,month,bottleUsed} = req.body;
+    //const  teacherId = req.user.id; // auth middleware
+
+    try{
+        //fetch distributed bottles from principlas table
+        const distributed = await DistributedBottles.findOne({classroomId,month});
+
+        if (!distributed){
+            return res.status(404).json({message:"No distributed bottle for the classroom"});
+        }
+
+        //validate bottleused
+        if(bottleUsed> distributed.bottleDistributed){
+              return res.status(404).json({message:"Bottles used can not be exceed the distributed bottles"});
+        }
+
+        //calculate remaining bottles
+           const bottleRemaining=  distributed.bottleDistributed - bottleUsed;
+
+           //update 
+
+           const record = await ClassroomBottles.findOneAndUpdate(
+            {classroomId,month},
+            {bottleUsed,bottleRemaining,updatedAt: new Date()},
+            {new:true,runValidators:true,upsert: true}
+           );
+       return res.status(200).json({ ClassroomBottles: record });
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({ message: "Server error" });
+    }
+}
