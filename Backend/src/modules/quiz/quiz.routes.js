cefb1c1c22801a80e243694b5729c19c1b4dfb6d
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-25 19:52:51 +0530

    feat(Quiz):Add delete endpoint

--- Backend/src/modules/quiz/quiz.routes.js
+++ Backend/src/modules/quiz/quiz.routes.js
@@ -1,9 +1,10 @@
 import express  from "express";
-import { createQuiz, getQuizzesByClassroom, UpdateQuiz } from "./quiz.controller.js";
+import { createQuiz, deleteQuiz, getQuizzesByClassroom, UpdateQuiz } from "./quiz.controller.js";
 const router = express.Router();
 
 router.post("/",createQuiz);
 router.get("/classroom/:classroomId",getQuizzesByClassroom);
 router.put('/:id',UpdateQuiz);
+router.delete('/:id',deleteQuiz);
 
 export default router;
