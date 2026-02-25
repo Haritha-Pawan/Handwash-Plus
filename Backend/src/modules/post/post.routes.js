cbea8367460673c7808b5433fae866613e9d92a6
lahirulakmal23 <lahirulakmal893@gmail.com>
2026-02-25 13:53:33 +0530

    add getmypost routes

--- Backend/src/modules/post/post.routes.js
+++ Backend/src/modules/post/post.routes.js
@@ -5,12 +5,12 @@ import {
   getAllPosts,
   updatePost,
   deletePost,
+  getMyPosts,
 } from "./post.controller.js";
-
-
 const router = express.Router();
 
-router.post("/", authMiddleware, createPost);
+router.post("/create", authMiddleware, createPost);
+router.get("/my-posts", authMiddleware, getMyPosts);
 router.get("/", getAllPosts);
 router.put("/:id", authMiddleware, updatePost);
 router.delete("/:id", authMiddleware, deletePost);
