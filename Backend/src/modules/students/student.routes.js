dc11cc52124a3937e2309dea0c82e51254e211c9
gayashagimhani <gayashagimhani12@gmail.com>
2026-02-25 22:34:21 +0530

    feat(student):add route getactivequizfor student

--- Backend/src/modules/students/student.routes.js
+++ Backend/src/modules/students/student.routes.js
@@ -2,12 +2,14 @@ import express  from "express";
 const router = express.Router();
 
 //Insert model
-import {addStudent, deleteStudent, getAllStudents,getById,updateStudent} from "./student.controller.js";
+import {addStudent, deleteStudent, getAllStudents,getById,updateStudent,getActiveQuizForStudent} from "./student.controller.js";
 
 router.get("/",getAllStudents);
 router.post("/",addStudent);
+router.get("/active" ,getActiveQuizForStudent);
 router.get("/:id",getById);
 router.put("/:id",updateStudent);
 router.delete("/:id",deleteStudent);
+
 
 export default router;
