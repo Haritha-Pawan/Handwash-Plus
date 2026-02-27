28d8efc5cc3aee50b09c02811907ba4ce2ef5314
Haritha-cd <harithapawan12@gmail.com>
2026-02-27 13:31:30 +0530

    remove User model password return issue

--- Backend/src/modules/grades/grade.routes.js
+++ Backend/src/modules/grades/grade.routes.js
@@ -3,12 +3,10 @@ import { Router } from "express";
 import gradeController                        from "./grade.controller.js";
 import { createGradesSchema, updateGradeSchema } from "./grade.validation.js";
 import { validate }                            from "../../@core/middleware/validate.middleware.js";
-import { protect, restrictTo }                 from "../../@core/middleware/auth.middleware.js";
 
 const router = Router();
 
-router.use(protect);
-router.use(restrictTo("admin"));
+
 
 router.post(
   "/",
